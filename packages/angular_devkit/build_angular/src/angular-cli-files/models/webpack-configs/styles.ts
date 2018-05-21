a8821d1d0b2b1bed3e53d78bd7bc6d12d8e7073c
Gabriel Schuster <github.com@actra.de>
2018-05-21 11:54:13 +0200

    Fix #945

--- packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/styles.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/styles.ts
@@ -189,7 +189,7 @@ export function getStylesConfig(wco: WebpackConfigOptions) {
   }
 
   // set base rules to derive final rules from
-  const baseRules: webpack.NewUseRule[] = [
+  const baseRules: webpack.RuleSetRule[] = [
     { test: /\.css$/, use: [] },
     {
       test: /\.scss$|\.sass$/, use: [{
